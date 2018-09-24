989a9c693e8cabd62b9e5ce11a2337ff23354ece
dayham soares <dayham.soares@gmail.com>
2018-09-24 19:19:02 -0300

    #

--- src/main.py
+++ src/main.py
@@ -11,6 +11,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 
 from credencial import cred
+"""crendencial.py contem um dict com as credenciais ex:
+cred = {'oauth_token': 'seu_token',
+        'client_id': 'seu client id'}"""
 
 
 class BoxMain(BoxLayout):
