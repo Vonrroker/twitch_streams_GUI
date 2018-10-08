291ad0591bb4c84157df922ab06ad06f61148a1c
dayham soares <dayham.soares@gmail.com>
2018-10-08 18:25:19 -0300

    add info token

--- src/main.py
+++ src/main.py
@@ -14,7 +14,9 @@ from kivy.uix.popup import Popup
 from credencial import cred
 """crendencial.py contem um dict com as credenciais ex:
 cred = {'oauth_token': 'seu_token',
-        'client_id': 'seu client id'}"""
+        'client_id': 'seu client id'}
+        
+info: https://dev.twitch.tv/docs/authentication/#getting-tokens """
 
 
 class BoxMain(BoxLayout):
