7f3f8626d14f3e46dfe517b9057d9a2f215b791f
Dayham Soares <dayhamsoares@gmail.com>
2020-09-29 17:03:31 -0300

    add seriaizer list streams

---
+++ app/utils/serializer_list_streams.py
@@ -0,0 +1,13 @@
+def serializer(stream_data):
+    streams = stream_data[1]["streams"]
+    list_streams_on = [
+        {
+            "channel_name": x["channel"]["name"],
+            "game": x["game"],
+            "viewers": x["viewers"],
+            "channel_status": x["channel"]["status"],
+            "preview_img": x["preview"]["large"],
+        }
+        for x in streams
+    ]
+    return list_streams_on
