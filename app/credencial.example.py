5851dbbebb18c04560e1a679fae003a14a3d4516
Dayham Soares <dayhamsoares@gmail.com>
2020-09-29 15:42:05 -0300

    update credencial.example

--- app/credencial.example.py
+++ app/credencial.example.py
@@ -1,3 +1,9 @@
+"""crendencial.py contem um dict com as credenciais ex:
+cred = {'oauth_token': 'seu_token',
+        'client_id': 'seu client id'}
+
+info: https://dev.twitch.tv/docs/authentication/#getting-tokens """
+
 cred = {
     "oauth_token": "",
     "client_id": "1vbir4oyot8xfyacob5lskwj4i89pj",
